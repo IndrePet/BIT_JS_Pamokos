80584e6d8b874b69437f285b82a7734b13cc99d5
IndrePet <indrepetrausku@gmail.com>
2022-03-09 21:14:24 +0200

    vinted clone api

--- 028/app.js
+++ 028/app.js
@@ -34,24 +34,68 @@ const authorList = document.querySelector('#authorList');
 fetch('https://picsum.photos/v2/list')
   .then((response) => response.json())
   .then((data) => {
-    let html = '';
     data.forEach((element) => {
-      html += `<li>${element.author}</li>`;
+      const li = document.createElement('li');
+      const author = document.createTextNode(element.author);
+      li.appendChild(author);
+      authorList.appendChild(li);
     });
-    authorList.innerHTML = html;
+    // let html = '';
+    // data.forEach((element) => {
+    //   html += `<li>${element.author}</li>`;
+    // });
+    // authorList.innerHTML = html;
   });
 
 console.log('------4-------');
 //padaryti nuotraukų albumą DONE!
 
 const galleryContainer = document.querySelector('.container');
+//uzkomentinta, nes sunkiai uzkrauna
 
-fetch('https://picsum.photos/v2/list')
+// fetch('https://picsum.photos/v2/list')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     let html = '';
+//     data.forEach((element) => {
+//       html += `<img src="${element.download_url}" height="100px">`;
+//     });
+//     galleryContainer.innerHTML = html;
+//   });
+
+console.log('------5-------');
+//VINTED
+
+const container2 = document.querySelector('.container2');
+
+fetch('https://in3.dev/vinted/api/products/id/14')
   .then((response) => response.json())
   .then((data) => {
-    let html = '';
-    data.forEach((element) => {
-      html += `<img src="${element.download_url}" height="100px">`;
+    const title2 = document.createElement('h2');
+    const titleText = document.createTextNode(data.title);
+    const desc = document.createElement('p');
+    const descText = document.createTextNode(data.desc);
+
+    //cia pirmas img issplice'inamas ir sukuriamas
+    const mainImgUrl = data.img[data.main_img];
+    data.img.splice(data.main_img, 1); //isimam, kad nesimaisytu prie visu kitu;
+    const img1 = document.createElement('img');
+    img1.setAttribute('src', mainImgUrl);
+    img1.classList.add('big');
+
+    desc.appendChild(descText);
+    title2.appendChild(titleText);
+
+    const div = document.createElement('div');
+    div.appendChild(img1);
+    data.img.forEach((image) => {
+      const img = document.createElement('img');
+      img.setAttribute('src', image);
+      img.classList.add('small');
+      div.appendChild(img);
     });
-    galleryContainer.innerHTML = html;
+
+    container2.appendChild(title2);
+    container2.appendChild(desc);
+    container2.appendChild(div);
   });
